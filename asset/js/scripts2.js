eb04c9052e8589f770fc80fd1273a833df386c89
levanduy01655857577 <37141432+levanduy01655857577@users.noreply.github.com>
2018-09-05 11:12:01 +0700

    form
    js form

---
+++ asset/js/scripts2.js
@@ -0,0 +1,48 @@
+if($('.modalLogin form').length > 0){
+	//blur form input
+	$('.modalLogin form .form-control').blur(function(){
+		var valForm = $(this).val().trim();
+		var idForm = $(this).attr('id');
+		if(valForm.length > 0){
+			$('#description-'+ idForm).addClass('hideDescriptionForm');
+			$('#group-'+ idForm).removeClass('borderDangerForm');
+		}else{
+			$('#description-'+ idForm).removeClass('hideDescriptionForm');
+			$('#group-'+ idForm).addClass('borderDangerForm');
+		}
+	});
+
+	$('#frmLogin form').submit(function(){
+		var allowSubmitForm = allowSubmitFormPopup('#frmLogin');
+		if(allowSubmitForm != 1) return false;
+	});
+
+	$('#frmRegisterEmail form').submit(function(){
+		var allowSubmitForm = allowSubmitFormPopup('#frmRegisterEmail');
+		if(allowSubmitForm != 1) return false;
+	});
+
+	$('#frmForgotPass form').submit(function(){
+		var allowSubmitForm = allowSubmitFormPopup('#frmForgotPass');
+		if(allowSubmitForm != 1) return false;
+	});
+}
+
+function allowSubmitFormPopup(nameForm){
+	var allowSubmitForm = 1;
+	$(nameForm +' .form-control').each(function(){
+		var valForm = $(this).val().trim();
+		var idForm = $(this).attr('id');
+		if(valForm.length > 0){
+			$('#description-'+ idForm).addClass('hideDescriptionForm');
+			$('#group-'+ idForm).removeClass('borderDangerForm');
+		}else{
+			$('#description-'+ idForm).removeClass('hideDescriptionForm');
+			$('#group-'+ idForm).addClass('borderDangerForm');
+
+			allowSubmitForm = 2;
+			return false;
+		}
+	});
+	return allowSubmitForm;
+}
